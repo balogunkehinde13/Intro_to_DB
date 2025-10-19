733089a5e8c2196d2b8b04afac4127aa8621a556
balogunkehinde13 <balogunraheem@gmail.com>
2025-10-19 22:15:52 +0100

    first commit

---
+++ task_4.sql
@@ -0,0 +1,13 @@
+-- Print the full description of the table 'books' 
+-- without using DESCRIBE or EXPLAIN
+
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE()
+  AND TABLE_NAME = 'books';
